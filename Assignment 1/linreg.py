5b92dbe9eadf70b516670164d1b43cde967f0d68
jimacasaet <macasaetjohn@gmail.com>
2018-02-07 07:43:43 +0800

    First commit

---
+++ Assignment 1/linreg.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+John Rufino Macasaet
+2013-18722
+CoE 197 Z
+"""
+
+import numpy as np;
+from scipy import linalg
---
+++ try.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 23:04:23 2018
+
+@author: Dingdong
+"""
+
+z = 2;
+e = 2.718;
+g = 1/(1 + e ** (-z) )
