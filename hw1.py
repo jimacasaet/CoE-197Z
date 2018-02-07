d92c07da3e59a97f5e20cee1bccb2bec013b2a80
jimacasaet <macasaetjohn@gmail.com>
2018-02-07 16:23:01 +0800

    edited hw1

--- hw1.py
+++ hw1.py
@@ -1,4 +1,6 @@
+import numpy as np
 from numpy import *
+
 def compute_error_for_line_given_points(b, m, points):
     totalError = 0
     for i in range(0, len(points)):
@@ -6,8 +8,8 @@ def compute_error_for_line_given_points(b, m, points):
         y = points[i, 1]
         totalError += (y - (m * x + b)) ** 2
     return totalError / float(len(points))
+    
 def step_gradient(b_current, m_current, points, learning_rate):
- 
     b_gradient = 0
     m_gradient = 0
     N = float(len(points))
@@ -19,14 +21,16 @@ def step_gradient(b_current, m_current, points, learning_rate):
     new_b = b_current - (learning_rate * b_gradient)
     new_m = m_current - (learning_rate * m_gradient)
     return [new_b, new_m]
+    
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
- 
     b = starting_b
     m = starting_m
     for i in range(num_iterations):
-        b, m = step_gradient(b,m, array(points), learning_rate
+        b, m = step_gradient(b,m, array(points), learning_rate)
     return [b, m]
-def run():
+
+#y = ax^3 + bx^2 + cx + d
+def run(a, b, c, d):
     points = genfromtext('data.csv', delimiter=',')
     learning_rate = 0.0001
     #y = mx + b (slope formula)
@@ -36,6 +40,7 @@ def run():
     
     [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
     
-    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
-if __name__ = '__main__':
-    run()
+    #print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+
+#if __name__ = '__main__':
+#    run()
